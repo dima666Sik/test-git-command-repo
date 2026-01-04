2663ac107fd171cbdae98bec590239da68eb6d00
dima666Sik <leniviy.demon.1@gmail.com>
2026-01-04 18:30:13 +0200

    test 1

--- src/main/java/ua/test/git/command/Main.java
+++ src/main/java/ua/test/git/command/Main.java
@@ -2,6 +2,6 @@ package ua.test.git.command;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! temp");
     }
 }
